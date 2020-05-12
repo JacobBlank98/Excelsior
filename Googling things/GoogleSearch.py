979e4b34c074f5dbc82065356ad19c76d8285d0f
jacobBlank98 <Jacob_Blank@hotmail.com>
2020-05-12 00:16:52 -0400

    Added GoogleImage Search Capabilities

---
+++ Googling things/GoogleSearch.py
@@ -0,0 +1,42 @@
+import os, tempfile
+from google_images_search import GoogleImagesSearch
+
+CurrentPath = os.getcwd()
+
+# if you don't enter api key and cx, the package will try to search
+# them from environment variables GCS_DEVELOPER_KEY and GCS_CX
+query = input("What Picture do you want to download?")
+
+
+
+gis = GoogleImagesSearch('AIzaSyBAcmq00cLgRfvmYsasuaqmuqKhe0HN6KI ', '004157383614410840261:hcpju7llgou')
+
+# example: GoogleImagesSearch('ABcDeFGhiJKLmnopqweRty5asdfghGfdSaS4abC', '012345678987654321012:abcde_fghij')
+
+# define search params:
+_search_params = {
+    'q': query,
+    'num': 1,
+    'safe': '',
+    'fileType': 'jpg',
+    'imgType': '',
+    'imgSize': '',
+    'imgDominantColor': ''
+}
+
+
+# this will search and download:
+gis.search(search_params=_search_params, path_to_dir= CurrentPath +  '/DownloadedPics/')
+
+# this will only search for images:
+#gis.search(search_params=_search_params)
+
+
+# this will search, download and resize:
+# gis.search(search_params=_search_params, path_to_dir='/path/', width=500, height=500)
+
+# search first, then download and resize afterwards:
+#gis.search(search_params=_search_params)
+# for image in gis.results():
+    # image.download('/path/')
+    # image.resize(500, 500)
---
+++ Googling things/ReadMe.txt
@@ -0,0 +1,25 @@
+Google image search README
+
+Following things are needed to run this:
+
+	python curses (I think this should be installed on Linux, on Windows use pip install windows-curses)
+	Google-Images-Search (python pip install Google-Images-Search)
+
+
+
+
+API Key:
+AIzaSyBAcmq00cLgRfvmYsasuaqmuqKhe0HN6KI 
+
+
+Search Engine Cx ID:
+004157383614410840261:hcpju7llgou
+
+
+
+
+Public URL:
+https://cse.google.com/cse?cx=004157383614410840261:hcpju7llgou	
+
+Tutorials:
+https://pypi.org/project/Google-Images-Search/
