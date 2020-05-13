88537246caa5d4ab42b29ac7407580ea657f6a55
Olaya-Sib <64824606+Olaya-Sib@users.noreply.github.com>
2020-05-12 23:25:38 -0400

    Update random_location.py

--- Random_Location_Generator/random_location.py
+++ Random_Location_Generator/random_location.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import random
 
-data = pd.read_csv("world-cities.csv")
-
 
 def random_location():
+    data = pd.read_csv("world-cities.csv")
     index = random.choice(range(len(data)))
     complete_name = str(data['name'][index]) + ' city, ' + str(data['country'][index])
     return complete_name
