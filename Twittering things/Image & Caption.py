cdffeabbab09c869d75cb9317b2588efb5672ca3
Olaya-Sib <64824606+Olaya-Sib@users.noreply.github.com>
2020-05-12 22:06:47 -0400

    Create Image & Caption.py
    python packages needed: 
    - tweepy
    
    Description:
    Takes user input for image file path & caption, and posts to Twitter.

---
+++ Twittering things/Image & Caption.py
@@ -0,0 +1,32 @@
+import tweepy
+
+# Authorization Tokens
+access_token = '1858668422-cQsfg6zTjRXnMErfbVkPTBQfRpz3wA0HTWbjUUB'
+access_token_secret = 'pWrVpJvGQB37p24e62e4oDNMopYYTqv6WgWkft5K6z1dR'
+consumer_key = 'pfoaSCz63ceQQWHoEJY3MEjAA'
+consumer_secret = 'siPmdQLwDrJN8RTkuf4tals7HFIAwnkjpmKLAXLnLAJGqIgEfg'
+
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Create API object
+api = tweepy.API(auth)
+
+
+def post_image_and_caption():
+    image = str(input("What's the image file path?"))
+    caption = str(input("What's the caption?"))
+
+    api = tweepy.API(auth)
+
+    # Upload image
+    media = api.media_upload(image)
+
+    # Post tweet with image
+    post_result = api.update_status(status=caption, media_ids=[media.media_id])
+
+
+if __name__ == "__main__":
+    post_image_and_caption()
