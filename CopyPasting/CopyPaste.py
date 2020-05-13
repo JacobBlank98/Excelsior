7cea7250d9032bde16755e85957e87d3832fa490
jacobBlank98 <Jacob_Blank@hotmail.com>
2020-05-13 00:40:48 -0400

    Added Copy Paste Functionality

---
+++ CopyPasting/CopyPaste.py
@@ -0,0 +1,39 @@
+import os
+from PIL import Image 
+ 
+ 
+ 
+def CopyPaste(TopImagePath, BottomImagePath, SavePath):
+
+    # open the images, get their sizes and make a copies so they're not affected
+    TopImage = Image.open(TopImagePath) 
+    TopImageCopy = TopImage.copy() 
+    TopWidth, TopHeight = TopImage.size   
+    BottomImage = Image.open(BottomImagePath) 
+    BottomWidth, BottomHeight = BottomImage.size
+    BottomImageCopy = BottomImage.copy() 
+
+    pasteWidth = int((BottomWidth / 2) - (TopWidth / 2))
+    pasteHeight = int(BottomHeight - TopHeight)
+    
+    # paste image giving dimensions 
+    BottomImageCopy.paste(TopImageCopy, (pasteWidth, pasteHeight)) 
+    
+    # save the image  
+    BottomImageCopy.save(SavePath) 
+
+
+
+
+if __name__ == "__main__":
+    
+    TopImagePath = input ("Enter Top Image path")
+    BottomImagePath = input("Enter Bottom Image path")
+    SavePath = input("Enter where to Save Image")
+    
+    if os.path.isfile(TopImagePath) == 0:
+        print("TopImage Directory does not exist")
+    elif os.path.isfile(BottomImagePath) == 0:
+        print("BottomImagePath Directory does not exist")
+    
+    CopyPaste(TopImagePath, BottomImagePath, SavePath)
