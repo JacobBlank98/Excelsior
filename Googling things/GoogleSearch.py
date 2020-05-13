edb0ad67ad440964e69142d2232ed1ede8bd96b9
jacobBlank98 <Jacob_Blank@hotmail.com>
2020-05-12 22:48:02 -0400

    Changed GoogleSearch.py to be in functions

--- Googling things/GoogleSearch.py
+++ Googling things/GoogleSearch.py
@@ -1,42 +1,51 @@
 import os, tempfile
 from google_images_search import GoogleImagesSearch
 
-CurrentPath = str(os.getcwd())
-
-# if you don't enter api key and cx, the package will try to search
-# them from environment variables GCS_DEVELOPER_KEY and GCS_CX
-query = input("What Picture do you want to download?")
 
 
+def GetGoogleImage(query, PathToDownload):
 
-gis = GoogleImagesSearch('AIzaSyBAcmq00cLgRfvmYsasuaqmuqKhe0HN6KI ', '004157383614410840261:hcpju7llgou')
+    # if you don't enter api key and cx, the package will try to search
+    # them from environment variables GCS_DEVELOPER_KEY and GCS_CX
+    # query = input("What Picture do you want to download?")
+    
+    gis = GoogleImagesSearch('AIzaSyBAcmq00cLgRfvmYsasuaqmuqKhe0HN6KI ', '004157383614410840261:hcpju7llgou')
+    
+    # define search params:
+    _search_params = {
+        'q': query,
+        'num': 1,
+        'safe': '',
+        'fileType': 'jpg',
+        'imgType': 'photo',
+        'imgSize': '',
+        'imgDominantColor': ''
+    }
+    
+    
+    
+    
+    # this will search, download and resize:
+    gis.search(search_params=_search_params, path_to_dir= PathToDownload, width=1000, height=500)
 
-# example: GoogleImagesSearch('ABcDeFGhiJKLmnopqweRty5asdfghGfdSaS4abC', '012345678987654321012:abcde_fghij')
+def DeleteImage(imagePath):
+    try:
+        os.remove(imagePath)
+        print("file removed")
+    except:
+        print("file not found")
 
-# define search params:
-_search_params = {
-    'q': query,
-    'num': 1,
-    'safe': '',
-    'fileType': 'jpg',
-    'imgType': 'photo',
-    'imgSize': '',
-    'imgDominantColor': ''
-}
-
-
+# os.remove(file) for file in os.listdir(CurrentPath) if file.endswith('.jpg')
 # this will search and download:
-gis.search(search_params=_search_params, path_to_dir= CurrentPath +  '/DownloadedPics/')
-
+#gis.search(search_params=_search_params, path_to_dir= CurrentPath +  '/DownloadedPics/')
+# 
 # this will only search for images:
 #gis.search(search_params=_search_params)
-
-
-# this will search, download and resize:
-# gis.search(search_params=_search_params, path_to_dir='/path/', width=500, height=500)
-
-# search first, then download and resize afterwards:
-#gis.search(search_params=_search_params)
+# # search first, then download and resize afterwards:
+# gis.search(search_params=_search_params)
 # for image in gis.results():
-    # image.download('/path/')
-    # image.resize(500, 500)
+    # image.download(CurrentPath +  '/DownloadedPics/')
+    # image.resize(1000, 500)
+    
+
+    
