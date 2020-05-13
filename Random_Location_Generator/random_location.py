d2ad6102f20f195b0a7e498603d2cfbda21725ef
Olaya-Sib <64824606+Olaya-Sib@users.noreply.github.com>
2020-05-12 23:10:01 -0400

    Update and rename Random_Country to Random_Location_Generator/random_location.py

---
+++ Random_Location_Generator/random_location.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import random
+
+data = pd.read_csv("world-cities_csv.csv")
+
+
+def random_location():
+    index = random.choice(range(len(data)))
+    complete_name = str(data['name'][index]) + ', ' + str(data['country'][index])
+    return complete_name
+
+
+if __name__ == "__main__":
+    random_location()
+
