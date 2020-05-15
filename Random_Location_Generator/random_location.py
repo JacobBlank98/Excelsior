77ae6a426c533fc9f8a2f6c2e1be7696cb61cde2
Olaya-Sib <64824606+Olaya-Sib@users.noreply.github.com>
2020-05-15 18:17:44 -0400

    Added random_image_path() function
    Returns the absolute path of a randomly selected image from the file.

--- Random_Location_Generator/random_location.py
+++ Random_Location_Generator/random_location.py
@@ -8,6 +8,10 @@ def random_location():
     complete_name = str(data['name'][index]) + ' city, ' + str(data['country'][index])
     return complete_name
 
+def random_image_path(file_directory):
+    choice = random.choice(os.listdir(str(file_directory)))
+    image_path = file_directory + choice
+    return image_path
 
 if __name__ == "__main__":
     random_location()
