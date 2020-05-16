2a1d65846157a68f65329da04d6a1913189c7c0e
Olaya-Sib <64824606+Olaya-Sib@users.noreply.github.com>
2020-05-15 22:20:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,31 @@
-import os, tempfile
-from google_images_search import GoogleImagesSearch
-from GoogleSearch import GetGoogleImage, DeleteImage
+from GoogleSearch import GetGoogleImage, DeleteImage, return_absolute_image_reference
 from Image_and_Caption import post_image_and_caption
-from random_location import random_location
-import os
-import tweepy
-import pandas as pd
+from random_location import random_location, random_image_path
+from CopyPasting import ResizeTopPicture, CopyPaste
 
-# only works if there is a single picture in file
-# must delete picture from file after using it
+# User Inputs for file/image paths
+City_Path = input("Enter directory of file to save background/city photos to.")
+Troy_Abed_Path = input("Enter directory of file to save foreground/TroyAbed")
+# photos to.")
+Resized_Path = input("Enter absolute path for resized image.")
+Merged_Path = input("Enter absolute path for merged image.")
 
-def return_absolute_image_reference(dir):
-    for filename in os.listdir(dir):
-        absolute_image_reference = str(dir) + '/' + str(filename)
-        return absolute_image_reference
+# Google Query
+query = random_location()  # random city name
+caption = ("Troy and Abed telepoooorting! This time to {}.".format(query))  # caption to be posted
 
+# top & bottom Images
+bottom_image_path = GetGoogleImage(query, PathToDownload=City_Path)
+top_image_path = random_image_path(Troy_Abed_Path)
 
-img_dir = input("What's the directory of the google_picture_downloads file?")
-query = random_location()
-PathToDownload = img_dir
+# resizing and merging
+ResizeTopPicture(top_image_path, bottom_image_path, Resized_Path)
+CopyPaste(top_image_path, bottom_image_path, Merged_Path)
 
-if os.path.isdir(PathToDownload) == 0:
-    print("That's not a directory!")
-else:
-    GetGoogleImage(query, PathToDownload)
+# posting
+post_image_and_caption(image=Merged_Path, caption=caption)
 
-absolute_image_reference = return_absolute_image_reference(dir=img_dir)  # absolute directory of image (sometimes
-# returns NoneType ??? )
-
-caption = ("Hi from {}.".format(query))  # caption to be posted
-
-post_image_and_caption(image=absolute_image_reference, caption=caption)
-
-DeleteImage(absolute_image_reference)  # Deletes image from google_images download file
+# deleting images
+DeleteImage(imagePath=City_Path)
+DeleteImage(imagePath=Resized_Path)
+DeleteImage(imagePath=Merged_Path)
