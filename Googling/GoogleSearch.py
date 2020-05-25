aa5db021e838d2b2246a18f46dc618af427f0bd8
Olaya-Sib <olayasibello@gmail.com>
2020-05-25 16:28:32 -0400

    Made some changes to main.py and googlig

--- Googling/GoogleSearch.py
+++ Googling/GoogleSearch.py
@@ -2,13 +2,12 @@ import os, tempfile
 from google_images_search import GoogleImagesSearch
 
 
-#Google and download a picture
+# Google and download a picture
 def GetGoogleImage(query, PathToDownload):
- 
     gis = GoogleImagesSearch('AIzaSyBAcmq00cLgRfvmYsasuaqmuqKhe0HN6KI ', '004157383614410840261:hcpju7llgou')
-    
+
     # GSImage gsImage
-    
+
     # define search params:
     _search_params = {
         'q': query,
@@ -19,14 +18,15 @@ def GetGoogleImage(query, PathToDownload):
         'imgSize': '',
         'imgDominantColor': ''
     }
-    
+
     # this will search, download and resize:
-    gis.search(search_params=_search_params, path_to_dir = PathToDownload)
+    gis.search(search_params=_search_params, path_to_dir=PathToDownload)
     for image in gis.results():
-        return image.path  
-        
-    
-#Remove The file
+        print(" \n" + image.path)
+        return image.path
+
+
+# Remove The file
 def DeleteImage(imagePath):
     try:
         os.remove(imagePath)
@@ -35,15 +35,14 @@ def DeleteImage(imagePath):
         print("file not found")
 
 
-#Use Functionality independently
+# Use Functionality independently
 if __name__ == "__main__":
 
     query = input("What image do you want to download?")
     PathToDownload = input("Where should I download?")
-    
+
     if os.path.isdir(PathToDownload) == 0:
         print("That's not a directory!")
     else:
         GetGoogleImage(query, PathToDownload)      
-    
   
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-from PIL import Image 
-from Googling.GoogleSearch import GetGoogleImage, DeleteImage
+from Googling.GoogleSearch import GetGoogleImage
 from Twitter.Image_and_Caption import post_image_and_caption
 from Random_Location_Generator.random_location import random_location, random_image_path
 from CopyPasting.CopyPaste import ResizeTopPicture, CopyPaste
@@ -15,7 +14,7 @@ Merged_Path = "{}/temp/MergedPic.jpg".format(cwd)
 # Google Query
 query = random_location()  # random city name
 modified_query = query.replace(" city,", ",")
-caption = ("Troy and Abed telepoooorting! This time to {}.".format(modified_query))  
+caption = ("Troy and Abed telepoooorting! This time to {}.".format(modified_query))
 
 # top & bottom Images
 bottom_image_path = GetGoogleImage(query, PathToDownload=City_Path)
@@ -33,4 +32,3 @@ os.remove(bottom_image_path)
 os.remove(Resized_Path)
 os.remove(Merged_Path)
 os.rmdir(City_Path)
-
